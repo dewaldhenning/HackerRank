b913b9265ef3d2e8695b374ca45ffa780b964555
Dewald Henning <34110648+dewaldhenning@users.noreply.github.com>
2020-01-29 16:44:37 +1100

    Update Eye_and_Identity.py

--- python3/numpy/Eye_and_Identity.py
+++ python3/numpy/Eye_and_Identity.py
@@ -1,3 +1,8 @@
 import numpy
 n,m = list(map(int,input().split()))
 print(numpy.eye(n,m,k=0))
+
+#-- OR
+
+import numpy
+print(str(numpy.eye(*map(int,input().split()))).replace('1',' 1').replace('0',' 0'))
